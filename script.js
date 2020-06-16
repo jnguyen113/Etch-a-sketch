f78cb4a4b83dbc2ec59516919116c61bc9f222fb
Johnathan <johnathan.nguyen1999@gmail.com>
2020-06-15 20:39:23 -0400

    basics of etch a sketch
    basics down

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const container = document.querySelector('.container');
+const frames = document.getElementsByClassName('.frames');
 
+
+
+function defaultRows(rows, cols){
+    //setting the rows being passed
+    container.style.setProperty('--grid-rows', rows);//all done through css
+    //setting the columns being passed
+    container.style.setProperty('--grid-cols', cols)
+    for(c = 0 ; c < (rows*cols) ; c++){
+        let cell = document.createElement('button');
+        cell.innerText = (c+1);
+        container.appendChild(cell).className = "grid-item";
+        
+        const buttons = document.querySelectorAll('button');//selecting all element button
+        buttons.forEach((button) => {//create var button for each buttons found
+            button.addEventListener('mouseover', function(){//create an event for the button
+                this.style.background = 'black';//set background to black when hovered over
+            });
+        });
+
+    }
+}
+function updateRows(rows, cols){
+    
+}
+
+defaultRows(16,16);
